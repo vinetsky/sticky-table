64f464cea32f95d670b4f8342b7eceb2d87bd200
Maksim Vinetsky <studio@yandex.ru>
2020-05-12 12:16:36 +0300

    Перекинул статусы наружу прогресс бара

--- js/courier.js
+++ js/courier.js
@@ -33,3 +33,39 @@
   document.querySelector('.order-scroll'),
   document.querySelector('.map-embed')
 );
+
+function firstFunc(arr) {
+  let buf, buf2;
+  let res = 'new Value: ';
+  for (i = 0; i < arr.length; i++) {
+    buf = arr[i].slice(0, 1);
+    buf2 = buf.toUpperCase();
+    res += buf2 + arr[i].substr(1, arr[i].length);
+    //console.log(i, res);
+  }
+  return res;
+}
+
+function handler1(el) {}
+
+console.log(firstFunc(['my', 'name', 'is', 'Trinity']));
+
+function handler2(el) {}
+
+//console.log(firstFunc([10, 20, 30], handler2));
+
+function handler3(el) {}
+
+/*console.log(
+  firstFunc(
+    [
+      { age: 45, name: 'Jhon' },
+      { age: 20, name: 'Aaron' },
+    ],
+    handler3
+  )
+);*/
+
+function handler4(el) {}
+
+//console.log(firstFunc(['abs', '123'], handler4));
